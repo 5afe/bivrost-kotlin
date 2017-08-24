5c57944404d3b7c39711ae5cfe426dea85b00363
fmrsabino <fredericomsabino@gmail.com>
2017-08-24 22:38:10 +0200

    Add UInt Generator (from UInt8 until UInt256)

--- src/main/kotlin/SolidityTypeGenerator.kt
+++ src/main/kotlin/SolidityTypeGenerator.kt
@@ -1,4 +1,5 @@
 import com.squareup.kotlinpoet.*
+import java.math.BigInteger
 import kotlin.reflect.KClass
 
 class SolidityTypeGenerator {
@@ -7,6 +8,21 @@ class SolidityTypeGenerator {
             Solidity.UInt256::class
     )
 
+    fun generateUInts() {
+        val kotlinFile = KotlinFile.builder("", "")
+        (8..256 step 8).forEach {
+            val uintClass = TypeSpec.classBuilder("UInt$it")
+                    .superclass(Solidity.UInt::class)
+                    .addSuperclassConstructorParameter("%1L, %2L", "value", it)
+                    .primaryConstructor(FunSpec.constructorBuilder().addParameter(
+                            ParameterSpec.builder("value", BigInteger::class).build()).build())
+                    .build()
+            kotlinFile.addType(uintClass)
+        }
+
+        kotlinFile.build().writeTo(System.out)
+    }
+    
     fun generateArrays() {
         val kotlinFile = KotlinFile.builder("", "")
         val solidityGeneratedObject = TypeSpec.objectBuilder("SolidityGenerated")
@@ -26,6 +42,5 @@ class SolidityTypeGenerator {
                 .primaryConstructor(FunSpec.constructorBuilder().addParameter(
                         ParameterSpec.builder("items", clazz, KModifier.VARARG).build()).build())
                 .build()
-
     }
 }
