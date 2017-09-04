0d2b4b1e49986e0ea7f0e40a06aac6bca9447695
fmrsabino <fredericomsabino@gmail.com>
2017-09-04 12:09:59 +0200

    Add tests for signed integer

--- src/test/kotlin/SolidityBaseTest.kt
+++ src/test/kotlin/SolidityBaseTest.kt
@@ -65,4 +65,61 @@ class SolidityBaseTest {
         assertEquals(true,
                 SolidityBase.decodeBool("0000000000000000000000000000000000000000000000000000000000000001"))
     }
+
+    @Test
+    fun testIntEncoding() {
+        assertEquals("0000000000000000000000000000000000000000000000000000000000000000",
+                Solidity.Int8(BigInteger.ZERO).encode())
+
+        assertEquals("0000000000000000000000000000000000000000000000000000000000000001",
+                Solidity.Int8(BigInteger.ONE).encode())
+
+        assertEquals("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+                Solidity.Int8(BigInteger.valueOf(-1)).encode())
+
+        assertEquals("000000000000000000000000000000000000000000000000000000000000007f",
+                Solidity.Int8(BigInteger.valueOf(127)).encode())
+
+        assertEquals("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff80",
+                Solidity.Int8(BigInteger.valueOf(-128)).encode())
+    }
+
+    @Test
+    fun testIntRange() {
+        (8..256 step 8).forEach {
+            val min = BigInteger.valueOf(2).pow(it - 1).negate()
+            val max = BigInteger.valueOf(2).pow(it - 1) - BigInteger.ONE
+            SolidityBase.Int(min, it)
+            SolidityBase.Int(max, it)
+            try {
+                SolidityBase.UInt(BigInteger.valueOf(2).pow(it), it)
+                fail("Expected InvalidBitLengthException")
+            } catch (e: InvalidBitLengthException) {
+            }
+
+            try {
+                SolidityBase.UInt(BigInteger.valueOf(2).pow(it), it)
+                fail("Expected InvalidBitLengthException")
+            } catch (e: InvalidBitLengthException) {
+            }
+        }
+    }
+
+    @Test
+    fun testIntDecoding() {
+        assertEquals(BigInteger.ZERO,
+                SolidityBase.decodeInt("0000000000000000000000000000000000000000000000000000000000000000"))
+
+        assertEquals(BigInteger.ONE,
+                SolidityBase.decodeInt("0000000000000000000000000000000000000000000000000000000000000001"))
+
+        assertEquals(BigInteger.valueOf(-1),
+                SolidityBase.decodeInt("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"))
+
+        assertEquals(BigInteger.valueOf(127),
+                SolidityBase.decodeInt("000000000000000000000000000000000000000000000000000000000000007f"))
+
+        assertEquals(BigInteger.valueOf(-128),
+                SolidityBase.decodeInt("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff80"))
+    }
 }
